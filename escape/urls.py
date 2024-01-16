cef805f2338405c785e82c68882996adaee913bb
Pai Khant Ko <kopaikhant@gmail.com>
2024-01-16 17:41:07 +0630

    rename project name into escape

--- escape/urls.py
+++ escape/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for delvron project.
+URL configuration for escape project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
